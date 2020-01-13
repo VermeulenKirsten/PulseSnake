f673e82c260a04a4804a420dee57d97fac5491b0
Kevin <kevin.tolpe@student.howest.be>
2020-01-13 12:06:10 +0100

    gitflow-feature-stash: codeUpdate

--- scripts/Models/Snake-2.js
+++ scripts/Models/Snake-2.js
@@ -29,46 +29,22 @@ function Snake(name, id, tail, direction, speed, color = '#00FF00') {
   this.Movesnake = function() {
     let tailend;
     let newhead;
-    let direction = this.Inputbuffer[0];
     if (this.Inputbuffer.length > 1) {
       this.Inputbuffer.shift();
-      direction = this.Inputbuffer[0];
-      if (direction == 'down') {
-        newhead = [this.Tail[0][0] + 1, this.Tail[0][1]];
-        tailend = this.Tail.pop();
-        this.Tail.unshift(newhead);
-      } else if (direction == 'up') {
-        newhead = [this.Tail[0][0] - 1, this.Tail[0][1]];
-        tailend = this.Tail.pop();
-        this.Tail.unshift(newhead);
-      } else if (direction == 'left') {
-        newhead = [this.Tail[0][0], this.Tail[0][1] - 1];
-        tailend = this.Tail.pop();
-        this.Tail.unshift(newhead);
-      } else if (direction == 'right') {
-        newhead = [this.Tail[0][0], this.Tail[0][1] + 1];
-        tailend = this.Tail.pop();
-        this.Tail.unshift(newhead);
-      }
-    } else {
-      if (direction == 'down') {
-        newhead = [this.Tail[0][0] + 1, this.Tail[0][1]];
-        tailend = this.Tail.pop();
-        this.Tail.unshift(newhead);
-      } else if (direction == 'up') {
-        newhead = [this.Tail[0][0] - 1, this.Tail[0][1]];
-        tailend = this.Tail.pop();
-        this.Tail.unshift(newhead);
-      } else if (direction == 'left') {
-        newhead = [this.Tail[0][0], this.Tail[0][1] - 1];
-        tailend = this.Tail.pop();
-        this.Tail.unshift(newhead);
-      } else if (direction == 'right') {
-        newhead = [this.Tail[0][0], this.Tail[0][1] + 1];
-        tailend = this.Tail.pop();
-        this.Tail.unshift(newhead);
-      }
     }
+    let direction = this.Inputbuffer[0];
+    tailend = this.Tail.pop();
+    if (direction == 'down') {
+      newhead = [this.Tail[0][0] + 1, this.Tail[0][1]];
+    } else if (direction == 'up') {
+      newhead = [this.Tail[0][0] - 1, this.Tail[0][1]];
+    } else if (direction == 'left') {
+      newhead = [this.Tail[0][0], this.Tail[0][1] - 1];
+    } else if (direction == 'right') {
+      newhead = [this.Tail[0][0], this.Tail[0][1] + 1];
+    }
+    this.Tail.unshift(newhead);
+
     // check if the snake ate the fruit
     if (fruit[0] == newhead[0] && fruit[1] == newhead[1]) {
       this.Tail.push(tailend);
@@ -105,24 +81,20 @@ function Snake(name, id, tail, direction, speed, color = '#00FF00') {
       }
     }
     // check if the snake went off screen and should die
-    try {
       // console.log('he should die right here: ', gamewidth / scalefactor - 1);
       // console.log(this.Tail[0][1]);
       if (this.Tail[0][0] <= -1 || this.Tail[0][0] > gameheight / scalefactor - 1) {
         console.log('u dead boi');
-        throw 'u dead';
       } else if (this.Tail[0][1] <= -1 || this.Tail[0][1] > gamewidth / scalefactor - 1) {
         console.log('u dead boi');
-        throw 'u dead';
       }
-    } catch {
-      this.Isalive = false;
-      console.log('u dead boi');
-      let message = new Paho.MQTT.Message(JSON.stringify(new Message('gameOver', { snake: this, method: 'went off screen' })));
-      message.destinationName = roomInfo.roomId;
-      mqtt.send(message);
+      // this.Isalive = false;
+      // console.log('u dead boi');
+      // let message = new Paho.MQTT.Message(JSON.stringify(new Message('gameOver', { snake: this, method: 'went off screen' })));
+      // message.destinationName = roomInfo.roomId;
+      // mqtt.send(message);
 
-      stop = true;
+      // stop = true;
     }
   };
 }
