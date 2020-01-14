fc6156de67e1bdcefbdb063d61e81d097032d3bf
Kirsten <kirsten.vermeulen@student.howest.be>
2020-01-14 20:33:29 +0100

    Redesign for the index page & adding 4 new pages

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+// ***********  Variables ***********
+
+let domPlay, domScoreboard, domCredits, domSettings;
+
+// ***********  DOM references ***********
+
+const doms = function() {
+  domPlay = document.querySelector(".js-play");
+  domScoreboard = document.querySelector(".js-scoreboard");
+  domCredits = document.querySelector(".js-credits");
+  domSettings = document.querySelector(".js-settings");
+};
+
+// ***********  Navigation ***********
+
+const goToPlay = function() {
+  window.location.href = "play.html";
+};
+
+const goToScoreboard = function() {
+  window.location.href = "scoreboard.html";
+};
+
+const goToCredits = function() {
+  window.location.href = "credits.html";
+};
+
+const goToSettings = function() {
+  window.location.href = "settings.html";
+};
+
+// ***********  Eventlisteners ***********
+
+const eventListeners = function() {
+  domPlay.addEventListener("click", goToPlay);
+  domScoreboard.addEventListener("click", goToScoreboard);
+  domCredits.addEventListener("click", goToCredits);
+  domSettings.addEventListener("click", goToSettings);
+};
+
+// *********** Init / DOMContentLoaded ***********
+
+const init = function() {
+  doms();
+  eventListeners();
+};
+
+document.addEventListener("DOMContentLoaded", init);
